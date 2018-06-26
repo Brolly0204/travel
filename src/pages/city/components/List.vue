fdc4ff5df364c8ce902abd7e2db491ead2def635
L.W.L <102495553@qq.com>
2018-06-26 18:46:29 +0800

    update List

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="list">
     <div class="area">
-      <div class="title">当前城市</div>
+      <div class="title border-topbottom">当前城市</div>
     </div>
     <div class="area">
-      <div class="title">热门城市</div>
+      <div class="title border-topbottom">热门城市</div>
     </div>
     <div class="area">
-      <div class="title">A</div>
+      <div class="title border-topbottom">A</div>
     </div>
   </div>
 </template>
@@ -19,5 +19,11 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  @import "~styles/variables.styl"
+  /*.title*/
+    /*line-height .4rem*/
+    /*background-color #eeeeee*/
+    /*padding-left .2rem*/
+    /*color #666*/
+    /*font-size 26rem*/
 </style>
